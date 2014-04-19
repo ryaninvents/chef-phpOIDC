efeb6cf77f830a1b9dd65c8a0871be9b2bc42820
Ryan Muller <ryan.a.muller@gmail.com>
2014-04-19 16:08:52 -0400

    Improved recipes

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,11 @@
 name             'phpOIDC'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Ryan Muller'
+maintainer_email 'ryan.a.muller@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures phpOIDC'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends 'apache2'
+depends 'mysql', '4.1.3'
+depends 'php'
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,22 @@
 # Cookbook Name:: phpOIDC
 # Recipe:: default
 #
-# Copyright 2014, YOUR_COMPANY_NAME
+# Released under GPLv3
 #
-# All rights reserved - Do Not Redistribute
-#
+
+include_recipe 'apache2'
+include_recipe 'git'
+
+include_recipe 'mysql::client'
+include_recipe 'mysql::server'
+
+include_recipe 'php'
+include_recipe 'php::module_mysql'
+include_recipe 'apache2::mod_php5'
+
+include_recipe 'php-extras::module_mdb2'
+include_recipe 'php-extras::module_mdb2_mysql'
+
+include_recipe 'php-extras::php_doctrine_orm'
+include_recipe 'php-extras::php_seclib'
+
